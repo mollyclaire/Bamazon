6318ff66cd4e58f6a3b70127e9ac1f4e28d6ea33
Molly Graham <mollyclaire.graham@gmail.com>
2019-06-28 16:16:08 -0500

    Styled the database content a bit. Still needs work. I also added the user prompts.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,16 +12,51 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    displayItems();
+    askBuyer();
+    connection.end();
   });
 
-function readSongs() {
+function displayItems() {
     console.log("Selecting all products...\n");
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         // Log all results of the SELECT statement
-        console.log(res);
-        connection.end();
-    });
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+          }
+          console.log("-----------------------------------");
+        });
+    
 }
 
-readSongs();
+function askBuyer() {
+    inquirer
+    .prompt([
+      {
+        name: "id",
+        type: "input",
+        message: "What product (id) would you like to buy?",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to purchase?",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+      }
+    ]).then(function(response) {
+        var chosenItem;
+    })
+}
+
